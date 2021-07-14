753a6711aba1e1588381c5e3ed0b85fe43105b3e
Yuta Miyasaka <miyaska_y@cyberowl.co.jp>
2021-07-14 18:25:06 +0900

    [A]Mountain.vueコンポーネント

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,14 +1,28 @@
 <template>
-  <div id="app">
+  <v-app id="app">
     <p>{{ message }}</p>
-  </div>
+    <Mountain/>
+
+    <v-alert
+      type="success"
+      width="400px"
+      color="green"
+      class="mx-auto"
+    >Alert created by Vuetify</v-alert>
+
+  </v-app>
 </template>
 
 <script>
+import Mountain from "./components/Mountain"
+
 export default {
+  components: {
+    Mountain
+  },
   data: function () {
     return {
-      message: "Hello Vue!"
+      message: "Hello Vue!",
     }
   }
 }
---
+++ app/javascript/components/Mountain.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="picture-wrapper">
+    <p>picture</p>
+    <img src="../../assets/images/mountain.jpg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Mountain'
+}
+</script>
+
+<style>
+p{
+  text-align: center;
+}
+.picture-wrapper{
+  margin: 0 auto;
+  max-width: 60%;
+}
+.picture-wrapper img{
+  width: 100%;
+}
+</style>
