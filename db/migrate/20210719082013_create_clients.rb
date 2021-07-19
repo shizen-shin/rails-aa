f52b74e3fe768ae944975ad9e0557ed71117c236
Yuta Miyasaka <hlaupcpky38@gmail.com>
2021-07-19 17:38:12 +0900

    [A]migration createsテーブル

--- db/migrate/20210719082013_create_clients.rb
+++ db/migrate/20210719082013_create_clients.rb
@@ -1,12 +1,12 @@
 class CreateClients < ActiveRecord::Migration[6.1]
   def change
     create_table :clients do |t|
-      t.string :pj_name
-      t.string :client_name
-      t.integer :status
-      t.date :order_date
-      t.bigint :price
-      t.text :memo
+      t.string :pj_name, null: false, defualt: ""
+      t.string :client_name, null:false, default: ""
+      t.integer :status, null: false, default: 0
+      t.date :order_date, null: false
+      t.bigint :price, null:false, defualt:0
+      t.text :memo, null: true
 
       t.timestamps
     end
