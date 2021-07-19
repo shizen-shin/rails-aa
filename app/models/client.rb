57f5c29f7ffa48a7a956fb3de9f6282f0950bbe9
Yuta Miyasaka <hlaupcpky38@gmail.com>
2021-07-19 17:45:41 +0900

    [A]clientモデル validation

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,11 @@
 class Client < ApplicationRecord
+  STATUS = { incoming: 0, in_progress: 1, finished: 2 }
+
+  enum status: STATUS
+
+  validates :pj_name, :client_name, exclusion: { in: [nil, ""] }, length: { in: 3..80 }
+  validates :client_name, :client_name, exclusion: { in: [nil, ""] }, length: { in: 3..80 }
+  validates :status, inclusion: { in: [0, 1, 2] }, exclusion: { in: [nil, ""] }
+  validates :price, numericality: { only_integer: true }
+
 end
