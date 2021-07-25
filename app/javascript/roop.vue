c1765e75b226224994316aff7f0290efe024a440
Yuta Miyasaka <hlaupcpky38@gmail.com>
2021-07-25 13:37:57 +0900

    [A]rails-db.tar 移管用

---
+++ app/javascript/roop.vue
@@ -0,0 +1,39 @@
+<template>
+  <div >
+    <div v-for="(val, idx) in result" :key=idx>
+      <div>{{ val }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      arr: ['aaa', 'bbb', 'ccc', 'ddd'],
+      isActive: false,
+    }
+  },
+  watch:{
+    arr:{
+      immediate: true,
+      handler(val){
+        let res = []
+        res = val.map( (elem, i) => {
+          if ( i == 1 ){
+            return  true
+          } else if (i == 3){
+            return false
+          }
+          return this.isActive
+        })
+        this.result = res
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
