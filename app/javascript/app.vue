76dba164f03824a440883b0596cf90f8ed15b4ec
yuta-miyasaka38 <hlaupcpky38@gmail.com>
2021-08-04 17:38:30 +0900

    [F]app.vue axios clients

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,33 +1,22 @@
 <template>
-  <v-app id="app">
-    <Header/>
-
-    <p class="page-title">{{ message }}</p>
-    <Mountain/>
-
-    <v-alert
-      type="success"
-      width="400px"
-      color="green"
-      class="mx-auto"
-    >Alert created by Vuetify</v-alert>
-
-  </v-app>
+  <div id="app">
+    {{ clients }}
+  </div>
 </template>
 
 <script>
-import Mountain from "./components/Mountain"
-import Header from "./components/Header"
+import axios from 'axios';
 
 export default {
-  components: {
-    Mountain,
-    Header
-  },
-  data: function () {
-    return {
-      message: "Hello Vue!",
+  data(){
+    return{
+      clients: []
     }
+  },
+  mounted(){
+    axios
+      .get('/api/pj1/clients')
+      .then(res => (this.clients = res))
   }
 }
 </script>
@@ -36,8 +25,5 @@ export default {
 p {
   font-size: 2em;
   text-align: center;
-}
-.page-title{
-  margin-top: 20px;
 }
 </style>
---
+++ app/javascript/withNavi.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app id="app">
+    <Header/>
+
+    <p class="page-title">{{ message }}</p>
+    <Mountain/>
+
+    <v-alert
+      type="success"
+      width="400px"
+      color="green"
+      class="mx-auto"
+    >Alert created by Vuetify</v-alert>
+
+  </v-app>
+</template>
+
+<script>
+import Mountain from "./components/Mountain"
+import Header from "./components/Header"
+
+export default {
+  components: {
+    Mountain,
+    Header
+  },
+  data: function () {
+    return {
+      message: "Hello Vue!",
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+.page-title{
+  margin-top: 20px;
+}
+</style>
