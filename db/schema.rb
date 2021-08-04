fdec4d9df21a938a4dfd26c52c9cc720539a18bb
yuta-miyasaka38 <hlaupcpky38@gmail.com>
2021-08-04 09:39:58 +0900

    [R]remove testsカラム

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_20_011142) do
+ActiveRecord::Schema.define(version: 2021_08_04_002221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,12 +44,6 @@ ActiveRecord::Schema.define(version: 2021_07_20_011142) do
     t.string "name"
     t.integer "age"
     t.integer "gender"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "tests", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
