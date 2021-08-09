718eea7572f45902c237d2fa5288c19fe32aa7f5
yuta-miyasaka38 <hlaupcpky38@gmail.com>
2021-08-09 10:05:23 +0900

    [A]Client.vue

--- app/javascript/components/clients/Client.vue
+++ app/javascript/components/clients/Client.vue
@@ -1,13 +1,53 @@
 <template>
-  
+  <div>
+      <dl>
+          <dt>client_name</dt>
+          <dd>{{ client.client_name }}</dd>
+          <dt>pj_name</dt>
+          <dd>{{ client.pj_name }}</dd>
+          <dt>status</dt>
+          <dd>{{ client.status }}</dd>
+          <dt>price</dt>
+          <dd>{{ client.price }}</dd>
+          <dt>order_date</dt>
+          <dd>{{ client.order_date }}</dd>
+      </dl>
+  </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  data(){
+    return{
+      client: []
+    }
+  },
+  mounted(){
+    axios
+      .get(`/api/pj1/clients/${this.$route.params.id}.json`)
+      .then(response => (this.client = response.data))
+  }
 }
 </script>
 
-<style>
 
+
+<style lang="scss" scoped>
+dl {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 10px;
+}
+dl dt {
+  width: 30%;
+  border: 1px solid gray;
+  padding: 10px;
+}
+dl dd {
+  width: 50%;
+  border: 1px solid gray;
+  padding: 10px;
+}
 </style>
