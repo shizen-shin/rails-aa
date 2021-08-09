00093d1d79b0a4cee6551a9fa84550f3466b426e
yuta-miyasaka38 <hlaupcpky38@gmail.com>
2021-08-09 18:02:05 +0900

    [F]ClientNewからClientForm.vueを切り出し

---
+++ app/javascript/components/clients/ClientForm.vue
@@ -0,0 +1,77 @@
+<template>
+  <form @submit.prevent="$emit('submit')">
+    <div class="error-wrapper" v-if="errors.length != 0">
+      <ul v-for="error in errors" :key="error">
+        <li><font color="red">{{ error }}</font></li>
+      </ul>
+    </div>
+    <div>
+      <label>client_name</label>
+      <input v-model="client.client_name" type="text">
+    </div>
+    <div>
+      <label>pj_name</label>
+      <input v-model="client.pj_name" type="text">
+    </div>
+    <div>
+      <label>status</label>
+      <select v-model="client.status">
+        <option>契約前</option>
+        <option>進行中</option>
+        <option>完了</option>
+      </select>
+    </div>
+    <div>
+      <label>order_date</label>
+      <input v-model="client.order_date" type="date">
+    </div>
+    <div>
+      <label>price</label>
+      <input v-model="client.price" type="number" min="0">
+    </div>
+    <div>
+      <label>memo</label>
+      <textarea v-model="client.memo"></textarea>
+    </div>
+    <button type="submit">送信</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props:{
+    client: {},
+    errors: "",
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+div:not(.error-wrapper){
+  display: flex;
+}
+label{
+  width: 15%;
+}
+input, select, textarea{
+  margin: 3px;
+  width: 30%;
+  border: 1px solid gray;
+}
+button{
+  margin: 10px;
+  display: inline-block;
+  padding: 0.5em 1em;
+  text-decoration: none;
+  background: #668ad8;
+  color: #FFF;
+  border-bottom: solid 4px #627295;
+  border-radius: 3px;
+  &:active{
+    -webkit-transform: translateY(4px);
+    transform: translateY(4px);
+    border-bottom: none;
+  }
+}
+</style>
--- app/javascript/components/clients/ClientNew.vue
+++ app/javascript/components/clients/ClientNew.vue
@@ -1,46 +1,15 @@
 <template>
-  <form @submit.prevent="createClient">
-    <div class="error-wrapper" v-if="errors.length != 0">
-      <ul v-for="error in errors" :key="error">
-        <li><font color="red">{{ error }}</font></li>
-      </ul>
-    </div>
-    <div>
-      <label>client_name</label>
-      <input v-model="client.client_name" value="初期値" type="text">
-    </div>
-    <div>
-      <label>pj_name</label>
-      <input v-model="client.pj_name" type="text">
-    </div>
-    <div>
-      <label>status</label>
-      <select v-model="client.status">
-        <option>契約前</option>
-        <option>進行中</option>
-        <option>完了</option>
-      </select>
-    </div>
-    <div>
-      <label>order_date</label>
-      <input v-model="client.order_date" type="date">
-    </div>
-    <div>
-      <label>price</label>
-      <input v-model="client.price" type="number" min="0">
-    </div>
-    <div>
-      <label>memo</label>
-      <textarea v-model="client.memo"></textarea>
-    </div>
-    <button type="submit">送信</button>
-  </form>
+  <ClientForm :errors='errors' :client="client" @submit="createClient" />
 </template>
 
 <script>
-import axios from 'axios';
+import ClientForm from './ClientForm.vue'
+import axios from 'axios'
 
 export default {
+  components:{
+    ClientForm: ClientForm,
+  },
   data(){
     return {
       client: {
@@ -75,30 +44,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-div:not(.error-wrapper){
-  display: flex;
-}
-label{
-  width: 15%;
-}
-input, select, textarea{
-  margin: 3px;
-  width: 30%;
-  border: 1px solid gray;
-}
-button{
-  margin: 10px;
-  display: inline-block;
-  padding: 0.5em 1em;
-  text-decoration: none;
-  background: #668ad8;
-  color: #FFF;
-  border-bottom: solid 4px #627295;
-  border-radius: 3px;
-  &:active{
-    -webkit-transform: translateY(4px);
-    transform: translateY(4px);
-    border-bottom: none;
-  }
-}
 </style>
